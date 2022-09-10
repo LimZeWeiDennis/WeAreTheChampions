f8f711d6254ec4f5fef6e647e6cec3512730a83e
LimZeWeiDennis <dennislimzw@gmail.com>
2022-09-10 11:50:17 +0800

    Added Registration page to the main app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import TextField from '@mui/material/TextField';
 import Registration from './modules/Registration';
+import RecordScore from './modules/RecordScore';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Registration/>
-        <p>Eh!</p>
-        
-      </header>
+        {/* <Registration/> */}
+        <RecordScore/>
+
+
     </div>
   );
 }
